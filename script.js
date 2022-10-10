227feb7169dcccfad2ccb29a12c53b47fbe4a31e
Eyyuep <eyuep.harputlu@gmail.com>
2022-10-11 01:34:14 +0200

    add a few ui elements add setupGrid with customizable grid size

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 let mousedown = false;
+let gridSize = document.getElementById("gridSize").value;
+let gridSizeLabel = `${gridSize} x ${gridSize}`;
+document.getElementById("gridSizeLabel").innerHTML = gridSizeLabel;
 
 document.body.addEventListener("mousedown", () => mousedown = true);
 document.body.addEventListener("mouseup", () => mousedown = false);
 
-const grid = document.createElement("div");
-grid.setAttribute("id", "grid");
 
-function setupGrid(){
-    for (let i = 0; i < 16; i++) {
+document.getElementById("gridSize").addEventListener("input", updateGridSize);
+
+const grid = document.querySelector("#grid");
+
+function updateGridSize(e){
+    gridSize = e.target.value;
+    gridSizeLabel = `${gridSize} x ${gridSize}`;
+    document.getElementById("gridSizeLabel").innerHTML = gridSizeLabel;
+    setupGrid(gridSize);
+}
+
+function setupGrid(gSize){
+    grid.innerHTML="";
+    let hw = Math.floor(800/gSize);
+    for (let i = 0; i < gSize; i++) {
         let gridElementParent = document.createElement("div");
         gridElementParent.setAttribute("id", `${i}`);
         gridElementParent.classList.add("gridElementParent");
         
-        for (let a = 0; a < 16; a++) {
+        for (let a = 0; a < gSize; a++) {
             let gridElementChild = document.createElement("div");
-            gridElementChild.setAttribute("id", `${i}.${a}`);
             gridElementChild.classList.add("gridElementChild");
+            gridElementChild.setAttribute("id", `${i}.${a}`);
+            gridElementChild.style.height = `${hw}px`;
+            gridElementChild.style.width = `${hw}px`;
             gridElementChild.addEventListener("mousedown", changeColor);
             gridElementChild.addEventListener("mouseover", changeColor);
             gridElementParent.appendChild(gridElementChild);
@@ -26,12 +42,11 @@ function setupGrid(){
 }
 
 function changeColor(e) {
-    if(e.type === "mouseover" && mousedown){
-        e.target.setAttribute("style", "background-color:black");
-    }else if(e.type === "mousedown") {
-        e.target.setAttribute("style", "background-color:black");
+    if(e.type === "mouseover" && !mousedown){
+        return;
     }
+    e.target.style.backgroundColor = "black";
 }
 
-setupGrid();
-document.querySelector("body").appendChild(grid);
+
+setupGrid(gridSize);
